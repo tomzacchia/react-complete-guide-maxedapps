8ccedff6348e48cfb17938a0aac8ca528687da26
tomzacchia <tomzacchia@live.com>
2022-01-13 17:39:27 -0500

    Add ExpenseItem date formatting

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,17 @@ import "./ExpenseItem.css";
 
 // we get props as the first argument to our component
 function ExpenseItem({ title, amount, date }) {
+  const monthLong = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{monthLong}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
