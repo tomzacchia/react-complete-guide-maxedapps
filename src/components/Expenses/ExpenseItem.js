f5b055207e9c318a63bf21ae8c53248065e97465
tomzacchia <tomzacchia@live.com>
2022-01-14 14:48:40 -0500

    useState in depth

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,17 +4,18 @@ import "./ExpenseItem.css";
 import { useState } from "react";
 
 function ExpenseItem({ title, amount, date }) {
-  /* COMMENT: Intro to useState
-    - useState(initialValue) returns 1) value of state, stored in our variable, 
-    and 2) function for updating our state value
-    - the returned method not only updates our state variable value but also
-    triggers a re-render of our component
+  /* COMMENT: useState in depth
+    - entire function body is re-evaluated for instance where state has changed
+    - expexpenseTitle = newState, is not what is happening when state changes
+    due to the fact that the entire function body re-evaluates as such using const
+    is fine. React will return the latest state from array returned by useState
+    - state is not re-initialized with props.title on subsequent re-renders
   */
   const [expenseTitle, setExpenseTitle] = useState(title);
 
   const clickHandler = () => {
     setExpenseTitle("New Title");
-    console.log(title); // consoles old value since updating state is async (gets scheduled)
+    console.log(title);
   };
 
   return (
