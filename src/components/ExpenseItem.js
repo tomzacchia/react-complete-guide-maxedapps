8d32ef5776a1b49f5316770ecc2d50b7e2494882
tomzacchia <tomzacchia@live.com>
2022-01-13 22:33:03 -0500

    Moving ExpenseItem date into separate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 // we get props as the first argument to our component
 function ExpenseItem({ title, amount, date }) {
-  const monthLong = date.toLocaleString("en-US", { month: "long" });
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{monthLong}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
