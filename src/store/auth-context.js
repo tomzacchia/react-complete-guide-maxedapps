a714d16c29f414ac260ce66ee7f86b91197e67fc
tomzacchia <tomzacchia@live.com>
2022-01-23 14:48:08 -0800

    CODE: adding IDE support for context auto-complete

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-/**
- * NOTE: React.createContext() returns a Context object
- * By default our consumers do not need to wrapped in a provider if we
- * consume the context when there is no 'Provider' in tree. However if we
- * do have a context Provider we need to defined 'value' attribute on
- * <contextObj.Provider value={} > </contextObj.Provider> component
- */
+// NOTE: we can pass in default context for auto-completion purposes
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
 });
 
 export default AuthContext;
