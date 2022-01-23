9a513247eca0a349558d7555f8298036a186a6d2
tomzacchia <tomzacchia@live.com>
2022-01-22 19:16:24 -0800

    NOTE: useEffect and uncommon side effect

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  /** NOTE: useEFfect and dependencies array
-   * If there are no dependencies our anonymous function would run once;
-   * when the App starts. Although we update the state within our effect
-   * triggering all subsequent Component logic to run our callback into
-   * useEffect won't run again due to there being no changes in our
-   * dependcy array.
-   *
-   * Our "effect" only executes after React has performed all DOM updates
-   */
   useEffect(() => {
     const userLoginInformation = localStorage.getItem("isLoggedIn");
     if (userLoginInformation === "1") setIsLoggedIn(true);
