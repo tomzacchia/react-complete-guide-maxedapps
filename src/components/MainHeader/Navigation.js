1317c01cd9d489a797e63ecfaa62813950884d11
tomzacchia <tomzacchia@live.com>
2022-01-23 14:42:10 -0800

    NOTE: Alternative way to consume contexts

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,36 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "store/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  // NOTE: pass in pointer to AuthContext instance, we get back context value
+  const context = useContext(AuthContext);
+
   return (
-    // NOTE: 2) this is one way to consume contexts from closest matching 'Provider' in tree
-    <AuthContext.Consumer>
-      {(context) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
