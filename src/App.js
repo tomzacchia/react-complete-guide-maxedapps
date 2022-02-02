af8bbe7014e149423b0a4655ff6fd913e82015d1
tomzacchia <tomzacchia@live.com>
2022-02-02 14:42:46 -0800

    set up repo for section-12

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "store/auth-context";
+import './App.css';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [otherState, setOtherState] = useState(0);
-
-  useEffect(() => {
-    const userLoginInformation = localStorage.getItem("isLoggedIn");
-    if (userLoginInformation === "1") setIsLoggedIn(true);
-  }, []);
-
-  const loginHandler = (email, password) => {
-    setIsLoggedIn(true);
-    localStorage.setItem("isLoggedIn", "1");
-  };
-
-  const logoutHandler = React.useCallback(() => {
-    setIsLoggedIn(false);
-  }, [setIsLoggedIn]);
-
-  function otherStateHandler() {
-    setOtherState((prevState) => {
-      return (prevState += 1);
-    });
-  }
-
   return (
-    <AuthContext.Provider
-      value={React.useMemo(
-        () => ({
-          isLoggedIn: isLoggedIn,
-          onLogout: logoutHandler,
-        }),
-        [isLoggedIn, logoutHandler]
-      )}
-    >
-      <MainHeader onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && (
-          <Home
-            onLogout={logoutHandler}
-            onOtherStateClick={otherStateHandler}
-            otherStateValue={otherState}
-          />
-        )}
-      </main>
-    </AuthContext.Provider>
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
   );
 }
 
--- src/store/auth-context.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// NOTE: we can pass in default context for auto-completion purposes
-const AuthContext = React.createContext({
-  isLoggedIn: false,
-  onLogout: () => {},
-});
-
-export default AuthContext;
