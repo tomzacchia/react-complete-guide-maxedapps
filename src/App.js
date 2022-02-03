8aa39013526ad4d4c5e922c4294566850590ae5f
tomzacchia <tomzacchia@live.com>
2022-02-02 16:17:57 -0800

    NOTE: React.memo()

--- src/App.js
+++ src/App.js
@@ -16,12 +16,6 @@ function App() {
     <div className="app">
       <Button onClick={toggleParagraph}> Toggle Paragraph </Button>
       <h1>Hi there!</h1>
-      {/* 
-        NOTE: In the case where an attribute's value never changes the function body
-        of our custom components still re-execute since <Demo /> is a function call
-        in the function body of <App />. The only difference is that JSX returned by <Demo />
-        might produce shadow DOM diffs depending on logic inside of <Demo />. 
-      */}
       <Demo show={false} />
     </div>
   );
--- src/components/Demo/demo.js
+++ src/components/Demo/demo.js
@@ -5,4 +5,19 @@ function Demo(props) {
   return <p>{props.show ? "This is new!" : ""}</p>;
 }
 
-export default Demo;
+/**
+ * NOTE: Memo tells react to check the props that our component, Demo, gets and
+ * compare previous and next values of the props. If previous === new, skip rexecution
+ * of component.
+ *
+ * CAUTION 1: memo doubles the memory used by our component, Demo, since React
+ * needs to store previous props to compare them to next props.
+ *
+ * CAUTION 2: 2nd arg to React.memo is a custom comparison function, which
+ * is helpful for comparing non-primitive types
+ *
+ * TRADE OFF: which is less performant? Comparing prev and next props or
+ * re-evaluating the component and its component tree. This trade-off
+ * varies based on every situation
+ */
+export default React.memo(Demo);
