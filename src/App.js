dd7ddb2a83d1f788cf55a5d6ff1be9b2b32590ba
tomzacchia <tomzacchia@live.com>
2022-02-02 16:32:10 -0800

    NOTE: useCallback to memoize functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 import Demo from "components/Demo/demo";
@@ -6,9 +6,17 @@ import Demo from "components/Demo/demo";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  function toggleParagraph() {
+  /**
+   * NOTE: useCallback tells React to store function body somewhere in internal
+   * memory and refernce that definition instead of passing down a different
+   * reference. usecallback is used with React.memo to prevent re-evaluating
+   * children components.
+   *
+   * NOTE: useState handlers are memoized, therefore not needed in depedency array
+   */
+  const toggleParagraph = useCallback(() => {
     setShowParagraph((prevState) => !prevState);
-  }
+  }, []);
 
   console.log("App running ");
 
--- src/components/Demo/demo.js
+++ src/components/Demo/demo.js
@@ -5,19 +5,4 @@ function Demo(props) {
   return <p>{props.show ? "This is new!" : ""}</p>;
 }
 
-/**
- * NOTE: Memo tells react to check the props that our component, Demo, gets and
- * compare previous and next values of the props. If previous === new, skip rexecution
- * of component.
- *
- * CAUTION 1: memo doubles the memory used by our component, Demo, since React
- * needs to store previous props to compare them to next props.
- *
- * CAUTION 2: 2nd arg to React.memo is a custom comparison function, which
- * is helpful for comparing non-primitive types
- *
- * TRADE OFF: which is less performant? Comparing prev and next props or
- * re-evaluating the component and its component tree. This trade-off
- * varies based on every situation
- */
 export default React.memo(Demo);
