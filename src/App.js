e5eb69b59411ca0839bba0993f3d3d9f1bf568bd
tomzacchia <tomzacchia@live.com>
2022-02-03 11:58:25 -0800

    CODE ONLY: async-await http request

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+const dummyMovies = [
+  {
+    id: 1,
+    title: "Some Dummy Movie",
+    openingText: "This is the opening text of the movie",
+    releaseDate: "2021-05-18",
+  },
+  {
+    id: 2,
+    title: "Some Dummy Movie 2",
+    openingText: "This is the second opening text of the movie",
+    releaseDate: "2021-05-19",
+  },
+];
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  // CODE ONLY:
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const moviesArray = data.results;
+    setMovies(moviesArray.map(mapMovieToNewKeys));
+  }
+
+  // useEffect();
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
 }
 
 export default App;
+
+function mapMovieToNewKeys(movie) {
+  return {
+    id: movie.episode_id,
+    title: movie.title,
+    openingText: movie.opening_crawl,
+    releaseData: movie.release_date,
+  };
+}
