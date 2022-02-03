0ba556b9daa3e1a0ed9ae8394fe3568ef73ae8ef
tomzacchia <tomzacchia@live.com>
2022-02-02 16:03:33 -0800

    NOTE: React and children re-evaluation. Additionally, DOM evaluations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
+import Demo from "components/Demo/demo";
 
 function App() {
-  /**
-   * NOTE: React compares virtual DOM diffs and passes those diffs to ReactDOM
-   * which takes care of rendering these changes. Clicking on Button only
-   * makes the second h1 flash in devTools
-   */
   const [showParagraph, setShowParagraph] = useState(false);
 
   function toggleParagraph() {
@@ -20,7 +16,13 @@ function App() {
     <div className="app">
       <Button onClick={toggleParagraph}> Toggle Paragraph </Button>
       <h1>Hi there!</h1>
-      {showParagraph && <h1>This is new!</h1>}
+      {/* 
+        NOTE: In the case where an attribute's value never changes the function body
+        of our custom components still re-execute since <Demo /> is a function call
+        in the function body of <App />. The only difference is that JSX returned by <Demo />
+        might produce shadow DOM diffs depending on logic inside of <Demo />. 
+      */}
+      <Demo show={false} />
     </div>
   );
 }
---
+++ src/components/Demo/demo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Demo(props) {
+  console.log("Demo rendering");
+  return <p>{props.show ? "This is new!" : ""}</p>;
+}
+
+export default Demo;
